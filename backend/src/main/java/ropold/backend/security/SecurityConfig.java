0199562a8cf60f4a8dd0a2ea58c961df702485c2
Robert Stolz <robert.stolz87@gmail.com>
2025-11-22 11:28:49 +0100

    AppUser Backend

--- backend/src/main/java/ropold/backend/security/SecurityConfig.java
+++ backend/src/main/java/ropold/backend/security/SecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import ropold.backend.model.AppUser;
+import ropold.backend.repository.AppUserRepository;
 
 
 import java.util.Collections;
@@ -28,6 +30,7 @@ public class SecurityConfig {
     @Value("${app.url}")
     private String appUrl;
 
+    private final AppUserRepository appUserRepository;
     private static final String ARCRAIDERS = "/api/arc-raiders-hub/**";
 
     @Bean
@@ -52,6 +55,30 @@ public class SecurityConfig {
                 .oauth2Login(o -> o.defaultSuccessUrl(appUrl));
 
         return http.build();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+        DefaultOAuth2UserService userService = new DefaultOAuth2UserService();
+
+        return (userRequest) -> {
+            OAuth2User githubUser = userService.loadUser(userRequest);
+
+            AppUser user = appUserRepository.findById(githubUser.getName())
+                    .orElseGet(() -> {
+                        AppUser newUser = new AppUser(
+                                githubUser.getName(),
+                                githubUser.getAttribute("login"),
+                                githubUser.getAttribute("name"),
+                                githubUser.getAttribute("avatar_url"),
+                                githubUser.getAttribute("html_url"),
+                                Collections.emptyList()
+                        );
+                        // hier kannst du die Rolle des Users setzen, z.B. "ROLE_USER"
+                        return appUserRepository.save(newUser);
+                    });
+            return githubUser;
+        };
     }
 
 }
